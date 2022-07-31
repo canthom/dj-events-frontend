a4de20856b3c984aeb2319dc99e0f5270d923ebc
Can <can.thomas@orange.fr>
2022-07-31 11:48:34 +0200

    Ajout de la page About et de la section Events

---
+++ pages/about.js
@@ -0,0 +1,9 @@
+export default function AboutPage() {
+  return (
+    <div>
+        <h1>About</h1>
+        <p>Cette application permet de trouver les derniers événements musicaux.</p>
+        <p>Version 1.0.0</p>
+    </div>
+  )
+}
---
+++ pages/events/add.js
@@ -0,0 +1,7 @@
+export default function add() {
+  return (
+    <div>
+        <h1>Add Event</h1>
+    </div>
+  )
+}
