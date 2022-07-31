8bba121b79cb993299e3f99491b2e82f3fe7e93e
Can <can.thomas@orange.fr>
2022-07-31 14:58:03 +0200

    Ajout du Layout, Header & Footer

--- pages/about.js
+++ pages/about.js
@@ -1,9 +1,11 @@
+import Layout from "../components/Layout"
+
 export default function AboutPage() {
   return (
-    <div>
+    <Layout title='About DJ Events'>
         <h1>About</h1>
         <p>Cette application permet de trouver les derniers événements musicaux.</p>
         <p>Version 1.0.0</p>
-    </div>
+    </Layout>
   )
 }
